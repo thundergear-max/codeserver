f2db19341337dcdb86f76206b6bdf845be12a9e3
Dragon-Gear <66841150+Dragon-Gear@users.noreply.github.com>
2022-03-12 18:41:52 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+var tag = document.createElement("p");
+var text = document.createTextNode("Tutorix is the best e-learning platform");
+tag.appendChild(text);
+var element = document.getElementById("new");
+element.appendChild(tag);
